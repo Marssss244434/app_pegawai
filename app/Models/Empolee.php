6136118148c7446ef175601537aeab590d5e9df4
Shidqi Nafis Amar <solusi10jsns@gmail.com>
2025-09-13 21:59:03 +0700

    penambahan branch baru dan employee

---
+++ app/Models/Empolee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Empolee extends Model
+{
+    protected $fillable = [
+        'nama_lengkap',
+        'email',
+        'nomor_telepon',
+        'tanggal_lahir',
+        'alamat',
+        'tanggal_masuk',
+        'status',
+        ];
+        
+}
