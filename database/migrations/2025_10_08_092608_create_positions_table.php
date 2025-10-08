9e25abcd3e64225613bf6ee74dcc9475d208d442
Shidqi Nafis Amar <solusi10jsns@gmail.com>
2025-10-08 17:10:53 +0700

    Penambahan Migration Semua tabel

--- database/migrations/2025_10_08_092608_create_positions_table.php
+++ database/migrations/2025_10_08_092608_create_positions_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('departements', function (Blueprint $table) {
+        Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_jabatan', 100);
+            $table->decimal('gaji_pokok', 10, 2);
             $table->timestamps();
-        });
+            });
     }
 
     /**
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('departements');
+        Schema::dropIfExists('positions');
     }
 };
